ecbc2f4b24cf0954d9dd709885ef90839d9c234f
MehdiTC <mehdi.touhamic@gmail.com>
2025-05-26 17:56:14 -0700

    Posthog - 3

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,9 +1,12 @@
 import posthog from 'posthog-js'
 
 if (typeof window !== 'undefined' && !posthog.__loaded) {
-  posthog.init('phc_qzpJn9wai6l4DhqVht2f6EPN6QfbEvqPy0hOnirPNUZ', {
-    api_host: 'https://app.posthog.com', // or your self-hosted URL
-  })
+  posthog.init(
+    process.env.NEXT_PUBLIC_POSTHOG_KEY || '', 
+    {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    }
+  )
 }
 
 export default posthog
